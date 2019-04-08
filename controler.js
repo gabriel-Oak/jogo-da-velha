37d77274b057eba82dc119c2dd8fc9e2c7a3bfe1
unknown <gabrielcarvalhocosta@live.com>
2019-04-08 15:28:02 -0300

    finalizado;

--- controler.js
+++ controler.js
@@ -11,8 +11,84 @@ function verifica(){
         } else {
             alert('Jogador 2 ganhou');
         }
+    } else if(marcados[3] == marcados[4] &&
+              marcados[4] == marcados[5] && 
+              marcados[3] != 0){
+        
+                if(marcados[3] == 'x'){
+                    alert('Jogador 1 ganhou');
+                } else {
+                    alert('Jogador 2 ganhou');
+                }
+    } else if(marcados[6] == marcados[7] &&
+              marcados[6] == marcados[8] && 
+              marcados[6] != 0){
+  
+          if(marcados[6] == 'x'){
+              alert('Jogador 1 ganhou');
+          } else {
+              alert('Jogador 2 ganhou');
+          }
+          //-------------------linhas
+    } else if(marcados[0] == marcados[4] &&
+            marcados[4] == marcados[8] && 
+            marcados[0] != 0){
+
+        if(marcados[0] == 'x'){
+            alert('Jogador 1 ganhou');
+        } else {
+            alert('Jogador 2 ganhou');
+        }
+    } else if(marcados[2] == marcados[4] &&
+              marcados[4] == marcados[6] && 
+              marcados[2] != 0){
+
+        if(marcados[2] == 'x'){
+            alert('Jogador 1 ganhou');
+        } else {
+            alert('Jogador 2 ganhou');
+        }
+        //--------------------------diagonais
+    } else if(marcados[0] == marcados[3] &&
+                marcados[3] == marcados[6] && 
+                marcados[0] != 0){
+
+        if(marcados[0] == 'x'){
+            alert('Jogador 1 ganhou');
+        } else {
+            alert('Jogador 2 ganhou');
+        }
+    } else if(marcados[0] == marcados[3] &&
+            marcados[3] == marcados[6] && 
+            marcados[0] != 0){
+
+        if(marcados[0] == 'x'){
+            alert('Jogador 1 ganhou');
+        } else {
+            alert('Jogador 2 ganhou');
+        }
+    } else if(marcados[1] == marcados[4] &&
+            marcados[4] == marcados[7] && 
+            marcados[1] != 0){
+
+        if(marcados[1] == 'x'){
+            alert('Jogador 1 ganhou');
+        } else {
+            alert('Jogador 2 ganhou');
+        }
+    }
+    else if(marcados[2] == marcados[5] &&
+            marcados[5] == marcados[8] && 
+            marcados[2] != 0){
+
+        if(marcados[2] == 'x'){
+            alert('Jogador 1 ganhou');
+        } else {
+            alert('Jogador 2 ganhou');
+        }
     }
 }
+    
 
 function marcar(quadrante){
     let quadro = document.getElementById(quadrante);
